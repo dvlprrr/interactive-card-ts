f7949bd4a743bc2901461972286dfb48511a7d48
Andrey <andreydaskal2002@gmail.com>
2023-12-07 17:22:56 +0300

    creating cardForm from beginning

--- src/components/CardForm/CardForm.tsx
+++ src/components/CardForm/CardForm.tsx
@@ -1,13 +1,3 @@
-import { ChangeEvent } from "react";
-import { CardNumberFormat } from "../../utils/payment";
-import {
-  CardFormInfoInput,
-  CardFormInfoWrapper,
-  CardFormInput,
-  CardFormLabel,
-  CardInfoItem,
-  WrapperCardForm,
-} from "./style";
 type PropType = {
   handleCardNumber: (value: string) => void;
   handleCardName: (value: string) => void;
@@ -17,36 +7,8 @@ export default function CardForm({
   handleCardName,
 }: PropType) {
   return (
-    <WrapperCardForm>
-      <CardFormLabel>cardholder name</CardFormLabel>
-      <CardFormInput
-        placeholder="e.g. Jane Appleseed  "
-        onChange={(event: ChangeEvent<HTMLInputElement>) => {
-          handleCardName(event.currentTarget.value);
-        }}
-      />
-      <CardFormLabel>card number</CardFormLabel>
-      <CardFormInput
-        placeholder="e.g. 1234 5678 9123 0000"
-        onChange={(event: ChangeEvent<HTMLInputElement>) => {
-          handleCardNumber(event.currentTarget.value);
-          CardNumberFormat(event.currentTarget);
-        }}
-      />
-      <CardFormInfoWrapper>
-        <CardInfoItem>
-          <CardFormLabel>Exp.Date</CardFormLabel>
-          <CardFormInfoInput />
-        </CardInfoItem>
-        <CardInfoItem>
-          <CardFormLabel>(MM/YY)</CardFormLabel>
-          <CardFormInfoInput />
-        </CardInfoItem>
-        <CardInfoItem>
-          <CardFormLabel>card number</CardFormLabel>
-          <CardFormInfoInput />
-        </CardInfoItem>
-      </CardFormInfoWrapper>
-    </WrapperCardForm>
+    <div>
+      <div></div>
+    </div>
   );
 }
