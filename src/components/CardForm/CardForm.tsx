c4228a9f48beffaed4da89784e8e34644616d471
Andrey <andreydaskal2002@gmail.com>
2023-12-08 13:19:24 +0300

    created markup for cardForm component

--- src/components/CardForm/CardForm.tsx
+++ src/components/CardForm/CardForm.tsx
@@ -1,3 +1,14 @@
+import { ChangeEvent } from "react";
+import {
+  CardFormDateInputWrapper,
+  CardFormInfoInput,
+  CardFormInfoItem,
+  CardFormInfoWrapper,
+  CardFormInput,
+  CardFormLabel,
+  WrapperCardForm,
+} from "./style";
+import { CardNumberFormat } from "../../utils/payment";
 type PropType = {
   handleCardNumber: (value: string) => void;
   handleCardName: (value: string) => void;
@@ -7,8 +18,35 @@ export default function CardForm({
   handleCardName,
 }: PropType) {
   return (
-    <div>
-      <div></div>
-    </div>
+    <WrapperCardForm>
+      <CardFormLabel>cardholder name</CardFormLabel>
+      <CardFormInput
+        onChange={(e: ChangeEvent<HTMLInputElement>) => {
+          handleCardName(e.target.value);
+        }}
+        placeholder="e.g. Jane Appleseed"
+      />
+      <CardFormLabel>card number</CardFormLabel>
+      <CardFormInput
+        onChange={(e: ChangeEvent<HTMLInputElement>) => {
+          CardNumberFormat(e.target);
+          handleCardNumber(e.target.value);
+        }}
+        placeholder="e.g. 1234 5678 9123 0000 "
+      />
+      <CardFormInfoWrapper>
+        <CardFormInfoItem>
+          <CardFormLabel>exp.date(mm/yy)</CardFormLabel>
+          <CardFormDateInputWrapper>
+            <CardFormInfoInput placeholder="MM" />
+            <CardFormInfoInput placeholder="YY" />
+          </CardFormDateInputWrapper>
+        </CardFormInfoItem>
+        <CardFormInfoItem>
+          <CardFormLabel>cvv</CardFormLabel>
+          <CardFormInfoInput placeholder="e.g. 123" />
+        </CardFormInfoItem>
+      </CardFormInfoWrapper>
+    </WrapperCardForm>
   );
 }
--- src/components/CardForm/style.ts
+++ src/components/CardForm/style.ts
@@ -1,3 +1,54 @@
 import styled from "styled-components";
 
+export const WrapperCardForm = styled.form`
+  width: 45%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+export const CardFormLabel = styled.label`
+  width: 65%;
+  font-size: 14px;
+  text-transform: uppercase;
+  letter-spacing: 2px;
+  margin-bottom: 10px;
+`;
+export const CardFormInput = styled.input`
+  box-sizing: border-box;
+  font-size: 20px;
+  font-family: "SpaceGrotesk", Arial, Helvetica, sans-serif;
+  font-weight: 500;
+  width: 65%;
+  padding: 10px 15px;
+  border: 2px solid hsl(270, 3%, 87%);
+  transition: border-image 0.3s ease-in-out;
+  &:focus {
+    border-color: hsl(278, 94%, 30%);
+  }
+  border-radius: 8px;
+  outline: none;
+  margin-bottom: 20px;
+`;
 
+export const CardFormInfoWrapper = styled.div`
+  width: 65%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  gap: 30px;
+`;
+export const CardFormInfoItem = styled.div`
+  width: 50%;
+  display: flex;
+  flex-direction: column;
+`;
+export const CardFormInfoInput = styled(CardFormInput)`
+  margin-bottom: 0;
+  width: 100%;
+`;
+export const CardFormDateInputWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 15px;
+`;
